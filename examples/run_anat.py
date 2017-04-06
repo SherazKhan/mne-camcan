d12095d848499ca35f97950540d971bf1e0867bd
sherazkhan <sheraz@nmr.mgh.harvard.edu>
2017-04-06 18:27:05 -0400

    combo

--- examples/run_anat.py
+++ examples/run_anat.py
@@ -1,33 +1,33 @@
 import os.path as op
 from nipype.interfaces.freesurfer import ReconAll
 from mne.parallel import parallel_func
-
+import glob
 
 subjects_dir = '/cluster/fusion/Sheraz/camcan/recons'
 camcan_path = '/cluster/transcend/MEG'
 
-subjects = ['CC110033', 'CC110037', 'CC110045']
-N_JOBS = 3
+
+N_JOBS = 72
+t1_files = op.join(camcan_path + '/camcan47/cc700/mri/pipeline/release004/BIDSsep/anat/sub-' + '*',
+             'anat', 'sub-' + '*' + '_T1w.nii.gz')
+t2_files = op.join(camcan_path + '/camcan47/cc700/mri/pipeline/release004/BIDSsep/anat/sub-' + '*',
+             'anat', 'sub-' + '*' + '_T2w.nii.gz')
+
+t1_files = glob.glob(t1_files)
+t2_files = glob.glob(t2_files)
 
 
-def process_subject_anatomy(subject):
-
-    t1_fname = op.join(camcan_path + '/camcan47/cc700/mri/pipeline/release004/BIDSsep/anat/sub-' + subject,
-             'anat', 'sub-' + subject + '_T1w.nii.gz')
-
-    t2_fname = op.join(camcan_path + '/camcan47/cc700/mri/pipeline/release004/BIDSsep/anat/sub-' + subject,
-             'anat', 'sub-' + subject + '_T2w.nii.gz')
-
+def process_subject_anatomy(t1):
     reconall = ReconAll()
-    reconall.inputs.subject_id = subject
+    reconall.inputs.subject_id = t1.split('/')[-3]
     reconall.inputs.directive = 'all'
     reconall.inputs.subjects_dir = subjects_dir
-    reconall.inputs.T1_files = t1_fname
-    reconall.inputs.T2_files = t2_fname
+    reconall.inputs.T1_files = t1
     reconall.run()
 
+
 parallel, run_func, _ = parallel_func(process_subject_anatomy, n_jobs=N_JOBS)
-parallel(run_func(subject) for subject in subjects)
+parallel(run_func(t1) for t1 in t1_files)
 
 
 
