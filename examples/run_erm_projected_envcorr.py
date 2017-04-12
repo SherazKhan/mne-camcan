444f96036502b04382ee598f64fd3b5c385b8b6f
sherazkhan <sheraz@nmr.mgh.harvard.edu>
2017-04-12 14:45:05 -0400

    combo

--- examples/run_erm_projected_envcorr.py
+++ examples/run_erm_projected_envcorr.py
@@ -17,14 +17,14 @@ subjects_dir = op.join(data_path,'recons')
 subject_dir = op.join(subjects_dir,subject)
 bem_dir = op.join(subject_dir,'bem')
 trans_file = op.join(data_path, 'trans',subject + '-trans.fif')
-labels_fname  = glob.glob(op.join(data_path, 'labels', '*.label'))
-labels = [mne.read_label(label, subject='fsaverageSK', color='r')
-          for label in labels_fname]
-for index, label in enumerate(labels):
-    label.values.fill(1.0)
-    labels[index] = label
-
-labels = [label.morph('fsaverageSK', subject, subjects_dir=subjects_dir) for label in labels]
+# labels_fname  = glob.glob(op.join(data_path, 'labels', '*.label'))
+# labels = [mne.read_label(label, subject='fsaverageSK', color='r')
+#           for label in labels_fname]
+# for index, label in enumerate(labels):
+#     label.values.fill(1.0)
+#     labels[index] = label
+#
+# labels = [label.morph('fsaverageSK', subject, subjects_dir=subjects_dir) for label in labels]
 
 event_id = 1
 event_overlap = 8
@@ -37,7 +37,7 @@ raw_fname = op.join(
     data_path, 'rest', 'sub-' + subject,'meg', 'rest_raw.fif')
 
 erm_fname = op.join(
-    data_path, 'emptyroom', '090707_raw.fif')
+    data_path, 'emptyroom', '090430_raw.fif')
 
 
 raw = mne.io.read_raw_fif(raw_fname, preload=True)
@@ -46,6 +46,9 @@ erm_raw = mne.io.read_raw_fif(erm_fname, preload=True)
 raw_length = (raw.last_samp-raw.first_samp)/raw.info['sfreq']
 raw.info['bads'] +=  [u'MEG2113', u'MEG1941', u'MEG1412', u'MEG2331']
 
+erm_raw_length = (erm_raw.last_samp-erm_raw.first_samp)/erm_raw.info['sfreq']
+erm_raw.info['bads'] +=  [u'MEG2113', u'MEG2112', u'MEG0512', u'MEG0141', u'MEG2533']
+
 
 raw = mne.preprocessing.maxwell_filter(raw, calibration=cal,
                                        cross_talk=ctc,
@@ -53,27 +56,50 @@ raw = mne.preprocessing.maxwell_filter(raw, calibration=cal,
                                        st_correlation=.96,
                                        origin=(0., 0., 0.04))
 
+erm_raw = mne.preprocessing.maxwell_filter(erm_raw, calibration=cal,
+                                       cross_talk=ctc,
+                                       st_duration=35.,
+                                       st_correlation=.96,
+                                       coord_frame='meg',
+                                       origin=(0., 0.013, -0.006))
+
 projs_ecg, ecg_events = compute_proj_ecg(raw, n_grad=1, n_mag=2)
 projs_eog, eog_events = compute_proj_eog(raw, n_grad=1, n_mag=2)
 
 raw.info['projs'] += projs_ecg
 raw.info['projs'] += projs_eog
 
+erm_raw.info['projs'] += projs_ecg
+erm_raw.info['projs'] += projs_eog
+
 raw.apply_proj()
+erm_raw.apply_proj()
+
 
 raw.filter(None, 40, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
            filter_length='auto', phase='zero', fir_window='hann')
 
 cov = mne.compute_raw_covariance(raw, tmin=0, tmax=None)
 
-raw.filter(14, 30, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
+erm_raw.filter(None, 40, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
            filter_length='auto', phase='zero', fir_window='hann')
 
+erm_cov = mne.compute_raw_covariance(erm_raw, tmin=0, tmax=None)
+
+raw.filter(14, 30, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
+           filter_length='auto', phase='zero', fir_window='hann')
 reject = dict(grad=1000e-13, mag=1.2e-12)
 events = mne.make_fixed_length_events(raw, event_id, duration=event_overlap, start=0, stop=raw_length-event_length)
 epochs = mne.Epochs(raw, events, event_id, 0,
                     event_length, baseline=None, preload=True, proj=False, reject=reject)
 epochs.resample(100.)
+
+erm_raw.filter(14, 30, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
+           filter_length='auto', phase='zero', fir_window='hann')
+erm_events = mne.make_fixed_length_events(erm_raw, event_id, duration=event_overlap, start=0, stop=raw_length-event_length)
+erm_epochs = mne.Epochs(erm_raw, erm_events, event_id, 0,
+                    event_length, baseline=None, preload=True, proj=False, reject=reject)
+erm_epochs.resample(100.)
 
 bem_fname = op.join(bem_dir, '%s-src.fif' % subject)
 src_fname = op.join(bem_dir, '%s-src.fif' % spacing)
@@ -81,8 +107,8 @@ src_fname = op.join(bem_dir, '%s-src.fif' % spacing)
 bem = mne.read_bem_solution(bem_fname)
 src = mne.read_source_spaces(src_fname)
 
-fwd = mne.make_forward_solution(raw_fname, trans=trans_file, src=src, bem=bem,fname=None, meg=True, eeg=False, n_jobs=2)
-inv = mne.minimum_norm.make_inverse_operator(raw.info, fwd, cov,loose=0.2, depth=0.8)
+fwd = mne.make_forward_solution(raw_fname, trans=trans_file, src=src, bem=bem, meg=True, eeg=False, n_jobs=2)
+inv = mne.minimum_norm.make_inverse_operator(raw.info, fwd, erm_cov,loose=0.2, depth=0.8)
 
 snr = 1.0  # use lower SNR for single epochs
 lambda2 = 1.0 / snr ** 2
