7301466ea6935da5259debeb74a9d457e6e202c2
dengemann <denis.engemann@gmail.com>
2017-04-15 20:05:23 -0400

    stuff

--- transit_scripts/plot_connectivity_power_envelopes.py
+++ transit_scripts/plot_connectivity_power_envelopes.py
@@ -4,8 +4,6 @@ import math
 
 import numpy as np
 
-import surfer
-
 import mne
 from mne.datasets.brainstorm import bst_resting
 
@@ -26,54 +24,6 @@ meg_dir = op.expanduser(
 
 spacing = 'ico5'
 
-labels_fname = glob.glob(
-    op.join(subjects_dir, 'fsaverage', 'label', '*.annot'))
-
-mne.datasets.fetch_hcp_mmp_parcellation(subjects_dir=subjects_dir,
-                                        verbose=True)
-
-labels = mne.read_labels_from_annot(
-    parc='HCPMMP1', subject='fsaverage', subjects_dir=subjects_dir)
-# labels = [ll for ll in labels if 'Brodmann' in ll.name]
-
-label_dict = {
-    lh.name[:-3]: (lh, rh) for lh, rh in zip(labels[::2], labels[1::2])}
-
-split_map = dict()
-for key, value in label_dict.items():
-    split_map[key] = int(math.ceil(np.max(
-        [label.vertices.shape[0] for label in value]) / 1e3))
-
-split_labels = list()
-for key, value in label_dict.items():
-    n_parts = split_map[key]
-    for label in value:
-        new_labels = label.split(n_parts) if n_parts > 1 else [label]
-        split_labels.extend(new_labels)
-
-colors = np.array([ll.color for ll in split_labels])
-colors += ((np.random.sample(len(colors)) - 0.5) * 0.015)[:, None]
-for ii, color in enumerate(colors):
-    split_labels[ii].color = color
-
-mne.write_labels_to_annot(
-    split_labels, subject='fsaverage', subjects_dir=subjects_dir,
-    parc='HCPMMP1_auto_split', overwrite=True)
-
-%matplotlib osx
-
-
-brain = surfer.Brain('fsaverage', surf='inflated', hemi='both', subjects_dir=subjects_dir)
-brain.add_annotation('HCPMMP1_auto_split')
-# sheraz_labels = glob.glob('/Users/dengemann/Dropbox/mne-camcan-data/labels/*label')
-# sheraz_labels = [mne.read_label(fname, subject='fsaverage') for fname in sheraz_labels]
-# sheraz_labels
-sheraz_label_dict = {
-    lh.name[:-3]: (lh, rh) for lh, rh in zip(sheraz_labels[::2], sheraz_labels[1::2])}
-
-sheraz_label_dict['inferiorparietal_1']
-
-[12].split(, freesurfer=True)
 raw = mne.io.read_raw_fif(raw_fname)
 raw.load_data()
 raw.set_channel_types({'EEG057': 'ecg', 'EEG058': 'eog'})
@@ -111,3 +61,5 @@ raw_noise.add_proj(ssp_ecg[:1] + ssp_eog[:1])
 raw_noise.load_data()
 raw_noise.filter(1, 200)
 noise_cov = mne.compute_raw_covariance(raw_noise, method='empirical')
+
+whos
